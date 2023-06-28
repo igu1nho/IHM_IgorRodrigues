f13c1703537ad7b627bc852c2baf5e8f3c784f57
Igor Luiz Rodrigues <89806466+igu1nho@users.noreply.github.com>
2023-06-28 09:58:40 -0300

    Add files via upload

---
+++ workspace_vscode/lab-spa/src/app/components/produto-tabela/produto-tabela.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ProdutoDTO } from 'src/app/dtos/produto.dto';
+import { ProdutoService} from 'src/app/services/produto.service';
+import { AlertService } from 'src/app/services/alert.service';
+
+@Component({
+  selector: 'app-produto-tabela',
+  templateUrl: './produto-tabela.component.html',
+  styleUrls: ['./produto-tabela.component.scss']
+})
+export class ProdutoTabelaComponent implements OnInit {
+
+  produtos: ProdutoDTO[] = [];
+
+  constructor(private service: ProdutoService, private alertService: AlertService) {
+    this.service.findAll()
+      .subscribe({
+        next: (data) => this.produtos = data,
+        error: (e) => console.error(e)
+      });
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
