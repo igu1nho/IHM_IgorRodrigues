1696e1fe8e94bc8548887512c1e18208731ae424
Igor Luiz Rodrigues <89806466+igu1nho@users.noreply.github.com>
2023-07-02 21:32:28 -0300

    Update produto-tabela.component.ts

--- workspace_vscode/lab-spa/src/app/components/produto-tabela/produto-tabela.component.ts
+++ workspace_vscode/lab-spa/src/app/components/produto-tabela/produto-tabela.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProdutoDTO } from 'src/app/dtos/produto.dto';
+import { AlertService } from 'src/app/services/alert.service';
 import { ProdutoService } from 'src/app/services/produto.service';
 
 @Component({
@@ -7,18 +8,23 @@ import { ProdutoService } from 'src/app/services/produto.service';
   templateUrl: './produto-tabela.component.html',
   styleUrls: ['./produto-tabela.component.scss']
 })
-export class ProdutoTabelaComponent implements OnInit {
-
+export class ProdutoTabelaComponent {
   produtos: ProdutoDTO[] = [];
 
-  constructor(private service: ProdutoService) {
-    this.service.findAll()
-      .subscribe({
-        next: (data) => this.produtos = data,
-        error: (e) => console.error(e)
-      });
-  }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  constructor(
+    private produtoService: ProdutoService,
+    private alertService: AlertService
+  ) {
+    this.produtoService.findAll().subscribe({
+      next: (data) => {
+        this.produtos = data;
+      },
+      error: (e) => {
+        let tit = 'Erro buscando produto';
+        let msg = e.message;
+        this.alertService.error(tit, msg);
+        console.error(e);
+      }
+    });
   }
 }
